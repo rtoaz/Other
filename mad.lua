97e86eb8a9b58c7c975bd3801937ffb8b0c7057e
rtoaz <2685700760@qq.com>
2025-10-26 02:05:06 +0800

    Add files via upload

--- mad.lua
+++ mad.lua
@@ -1,17 +1,9 @@
--- 设置团队
 game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SetTeam", "Police")
-
 wait(.75)
-
--- 每帧都创建新的连接（如您要求的重复连接）
-while true do
-    game:GetService("RunService").RenderStepped:Connect(function()
-        for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-            if v.Name == "Handcuffs" then 
-                v.Parent = game:GetService("Players").LocalPlayer.Character
-            end
-        end
-        game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
-    end)
-    wait() -- 短暂等待，避免完全卡死
+game:GetService("RunService").RenderStepped:Connect(function()
+for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+   if v.Name == "Handcuffs" then v.Parent = game:GetService("Players").LocalPlayer.Character
+   end
 end
+game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
+end)
