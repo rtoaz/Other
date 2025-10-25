e3c25fc31ecfad66f684fcf7457c33d551cdb8db
rtoaz <2685700760@qq.com>
2025-10-26 02:13:37 +0800

    Update mad.lua

--- mad.lua
+++ mad.lua
@@ -1,9 +1,16 @@
 game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SetTeam", "Police")
 wait(.75)
+
+-- 使用 RenderStepped 但修复了语法错误
 game:GetService("RunService").RenderStepped:Connect(function()
-for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-   if v.Name == "Handcuffs" then v.Parent = game:GetService("Players").LocalPlayer.Character
-   end
-end
-game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
+    -- 装备手铐
+    for i, v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+        if v.Name == "Handcuffs" then
+            v.Parent = game:GetService("Players").LocalPlayer.Character
+            break
+        end
+    end
+    
+    -- 执行弹出操作
+    game:GetService("ReplicatedStorage").Event:FireServer("Eject", game:GetService("Players").LocalPlayer)
 end)
